8cefc448e440edd75f4882a3472def3e5ab3eb4c
andrewwindfall <andrew@windfallstudio.com>
2019-05-24 16:11:11 -0600

    support textbox components

--- src/views/dashboard/functions/transfer.blade.php
+++ src/views/dashboard/functions/transfer.blade.php
@@ -67,12 +67,49 @@
       $.each($('.component-write'), function() {
         var forms = $(this).find(".forms");
         var preview = $(this).find(".preview");
+        if($(forms).find("input[name='input1[]']").length)
+        {
           $(preview).find('.input1').html($(forms).find("input[name='input1[]']").val());
+        }
+        else {
+          $(preview).find('.input1').html($(forms).find("textarea[name='input1[]']").val());
+        }
+        if($(forms).find("input[name='input2[]']").length)
+        {
           $(preview).find('.input2').html($(forms).find("input[name='input2[]']").val());
+        }
+        else {
+          $(preview).find('.input2').html($(forms).find("textarea[name='input2[]']").val());
+        }
+        if($(forms).find("input[name='input3[]']").length)
+        {
           $(preview).find('.input3').html($(forms).find("input[name='input3[]']").val());
+        }
+        else {
+          $(preview).find('.input3').html($(forms).find("textarea[name='input3[]']").val());
+        }
+        if($(forms).find("input[name='input4[]']").length)
+        {
           $(preview).find('.input4').html($(forms).find("input[name='input4[]']").val());
+        }
+        else {
+          $(preview).find('.input4').html($(forms).find("textarea[name='input4[]']").val());
+        }
+        if($(forms).find("input[name='input5[]']").length)
+        {
           $(preview).find('.input5').html($(forms).find("input[name='input5[]']").val());
+        }
+        else {
+          $(preview).find('.input5').html($(forms).find("textarea[name='input5[]']").val());
+        }
+        if($(forms).find("input[name='input6[]']").length)
+        {
           $(preview).find('.input6').html($(forms).find("input[name='input6[]']").val());
+        }
+        else {
+          $(preview).find('.input6').html($(forms).find("textarea[name='input6[]']").val());
+        }
+
       });
     }
 
